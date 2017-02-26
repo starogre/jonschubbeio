a8d8a33529fc6390d705a823ac5ddb13c44f64c5
Jon Schubbe <jonshoob@gmail.com>
2017-02-26 17:10:56 -0500

    port stuff for heroku fix

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
 
+//app.set('port', (process.env.PORT || 2000));
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname+'/client/index.html');
@@ -12,5 +13,14 @@ app.use('/client',express.static(__dirname+'/client'));
 
 app.use('/client/scripts', express.static(__dirname + '/node_modules/vue/dist/'));
 
-serv.listen(2000);
+//heroku deploy
+// app.listen(app.get('port'), function() {
+//   console.log('Node app is running on port', app.get('port'));
+// });
+
+serv.listen(process.env.PORT || 2000);
+
+//local testing
+//serv.listen(2000);
+
 console.log("Server started.");
